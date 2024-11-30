56d7d2ae379b1784626c533ecbbc12124c087eec
ganhaque <ganhaque.main@gmail.com>
2024-11-29 22:50:57 -0600

    wip: course_edit_form

--- src/app/course_edit_form.tsx
+++ src/app/course_edit_form.tsx
@@ -34,6 +34,7 @@ import { useState } from "react";
 import { SheetClose } from "@/components/ui/sheet"
 import { Course, use_filter_context } from "@/app/data_provider"
 import { SemesterSelect } from "@/components/select/semester"
+import { DepartmentSelect } from "@/components/select/department"
 
 type CourseEditFormProps = {
   current_course: Course;
@@ -42,39 +43,37 @@ type CourseEditFormProps = {
 // TODO: make create course form where it is the same but 
 export function CourseEditForm({current_course} : CourseEditFormProps) {
   const [is_semester_select_open, set_is_semester_select_open] = useState(false);
-  const [new_semester, set_new_semester] = useState(current_course.semester_title);
+  const [is_department_select_open, set_is_department_select_open] = useState(false);
 
-  const [new_available, set_new_available] = useState(current_course.available);
-
-  const {
-    current_schedule,
-    set_current_schedule,
-    courses,
-    semester_filter,
-    set_semester_filter,
-    department_filter,
-    set_department_filter,
-
-    semesters,
-  } = use_filter_context();
+  /* const { */
+  /*   current_schedule, */
+  /*   set_current_schedule, */
+  /*   courses, */
+  /*   semester_filter, */
+  /*   set_semester_filter, */
+  /*   department_filter, */
+  /*   set_department_filter, */
+  /**/
+  /*   semesters, */
+  /* } = use_filter_context(); */
 
   const formSchema = z.object({
     semester: z
     .string(),
     department: z
     .string(),
-    // TODO:
-    available: z
-    .number({message: "section has to be a positive integer"})
-    .nonnegative({message: "available must be greater than or equal to 0"}),
-    // TODO:
-    enrollment: z
-    .number({message: "enrollment has to be a positive integer"})
-    .nonnegative({message: "enrollment must be greater than or equal to 0"}),
-    // TODO:
+    course_number: z
+    .number({message: "course number has to be a positive integer"})
+    .nonnegative({message: "course number must be greater than or equal to 0"}),
     section: z
     .number({message: "section has to be a positive non-zero integer"})
     .positive({message: "section number must be greater than 0"}),
+    available: z
+    .number({message: "section has to be a positive integer"})
+    .nonnegative({message: "available must be greater than or equal to 0"}),
+    enrollment: z
+    .number({message: "enrollment has to be a positive integer"})
+    .nonnegative({message: "enrollment must be greater than or equal to 0"}),
     // TODO:
     credit_hour: z
     .string(),
@@ -91,8 +90,13 @@ export function CourseEditForm({current_course} : CourseEditFormProps) {
     resolver: zodResolver(formSchema),
     defaultValues: {
       semester: current_course.semester_title,
+      department: current_course.department_title,
+      course_number: current_course.course_number,
+      section: current_course.section,
       title: current_course.course_title,
       instructor: current_course.instructor_name ? current_course.instructor_name : "",
+      available: current_course.available ? current_course.available : 0,
+      enrollment: current_course.enrollment ? current_course.enrollment : 0,
     },
   })
 
@@ -128,6 +132,86 @@ export function CourseEditForm({current_course} : CourseEditFormProps) {
           />
           <FormField
             control={form.control}
+            name="department"
+            render={({ field }) => (
+              <FormItem className="flex gap-4 items-center">
+                <FormLabel>Department:</FormLabel>
+                <FormControl>
+                  <DepartmentSelect
+                    current_select={field.value}
+                    is_open={is_department_select_open}
+                    set_is_open={set_is_department_select_open}
+                    on_select={field.onChange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex gap-4">
+            <FormField
+              control={form.control}
+              name="course_number"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Number:</FormLabel>
+                  <FormControl>
+                    <Input className="w-16"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="section"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Section:</FormLabel>
+                  <FormControl>
+                    <Input className="w-12"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="available"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Available:</FormLabel>
+                  <FormControl>
+                    <Input className="w-14"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="enrollment"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Enrollment:</FormLabel>
+                  <FormControl>
+                    <Input className="w-14"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormField
+            control={form.control}
             name="title"
             render={({ field }) => (
               <FormItem>
@@ -148,7 +232,7 @@ export function CourseEditForm({current_course} : CourseEditFormProps) {
               <FormItem>
                 <FormLabel>Instructor:</FormLabel>
                 <FormControl>
-                  <Input
+                  <Input className="w-48"
                     {...field}
                   />
                 </FormControl>
